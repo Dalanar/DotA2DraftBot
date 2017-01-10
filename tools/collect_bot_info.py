6ff222782a6c40d99004eeee101ca912e6e8dd11
Dalanar <legolas001@mail.ru>
2017-01-10 21:48:31 +0700

    Fixed #2: Disabled not implemented heroes (http://dota2.gamepedia.com/Bots#Lists_of_Heroes_used_by_bots)

--- tools/collect_bot_info.py
+++ tools/collect_bot_info.py
@@ -1,4 +1,45 @@
 import vdf
+implemented_bots = set([
+    'npc_dota_hero_axe',
+    'npc_dota_hero_bane',
+    'npc_dota_hero_bounty_hunter',
+    'npc_dota_hero_bloodseeker',
+    'npc_dota_hero_bristleback',
+    'npc_dota_hero_chaos_knight',
+    'npc_dota_hero_crystal_maiden',
+    'npc_dota_hero_dazzle',
+    'npc_dota_hero_death_prophet',
+    'npc_dota_hero_dragon_knight',
+    'npc_dota_hero_drow_ranger',
+    'npc_dota_hero_earthshaker',
+    'npc_dota_hero_jakiro',
+    'npc_dota_hero_juggernaut',
+    'npc_dota_hero_kunkka',
+    'npc_dota_hero_lich',
+    'npc_dota_hero_lina',
+    'npc_dota_hero_lion',
+    'npc_dota_hero_luna',
+    'npc_dota_hero_necrolyte',
+    'npc_dota_hero_omniknight',
+    'npc_dota_hero_oracle',
+    'npc_dota_hero_phantom_assassin',
+    'npc_dota_hero_pudge',
+    'npc_dota_hero_razor',
+    'npc_dota_hero_sand_king',
+    'npc_dota_hero_nevermore',
+    'npc_dota_hero_skywrath_mage',
+    'npc_dota_hero_sniper',
+    'npc_dota_hero_sven',
+    'npc_dota_hero_tidehunter',
+    'npc_dota_hero_tiny',
+    'npc_dota_hero_vengefulspirit',
+    'npc_dota_hero_viper',
+    'npc_dota_hero_warlock',
+    'npc_dota_hero_windrunner',
+    'npc_dota_hero_witch_doctor',
+    'npc_dota_hero_skeleton_king',
+    'npc_dota_hero_zuus',    
+])
 heroes = vdf.load(open(r'D:\games\steamapps\common\dota 2 beta\game\dota\scripts\npc\npc_heroes.txt'))
 with open('hero_bot_data.lua', 'w') as output:
     # Write module exporting stuff #1
@@ -39,7 +80,7 @@ with open('hero_bot_data.lua', 'w') as output:
                 this_hero_type_raw = data['Bot']['HeroType'].split('|')
                 for hero_type in this_hero_type_raw:
                     this_hero_type |= hero_type_ids[hero_type.strip()]
-                if 'Loadout' not in data['Bot']:
+                if ('Loadout' not in data['Bot']) or (name not in implemented_bots):
                     not_supported_list.append(human_name)
                 else:
                     output.write('    [\'%s\'] = {[\'HeroType\'] = %s, [\'LaningInfo\'] = {%s}},\n' % (name, this_hero_type, ', '.join(laning_info)))
